72932087b61cec341f9eeafb8898f076a25298d9
mahak <mahakhammessi7@gmail.com>
2026-03-17 00:44:45 +0100

    connect to render backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://pfe-backend-9eec.onrender.com/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
